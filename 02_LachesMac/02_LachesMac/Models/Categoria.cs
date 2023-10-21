1ece8c2ab49aa3f63185809f04da4bd532697c6b
Vinicius Loss <viniciusvianaloss@gmail.com>
2023-10-21 14:05:58 -0300

    Definição de classes de domínio.

---
+++ 02_LachesMac/02_LachesMac/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace _02_LachesMac.Models
+{
+	public class Categoria
+	{
+		public int CategoriaId { get; set; }
+		public string CategoriaNome { get; set; }
+		public string Descricao { get; set; }
+
+		public List<Lanche> Lanches {get; set;}
+    }
+}
+
