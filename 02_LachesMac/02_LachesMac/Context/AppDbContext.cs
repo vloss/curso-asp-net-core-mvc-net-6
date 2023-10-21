3a1c85f94b3eea6363eefb3ee6457f155ef3811a
Vinicius Loss <viniciusvianaloss@gmail.com>
2023-10-21 14:35:59 -0300

    Inclusão de AppDbContext para conexão com banco.

---
+++ 02_LachesMac/02_LachesMac/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using _02_LachesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _02_LachesMac.Context
+{
+	public class AppDbContext : DbContext
+	{
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
+
