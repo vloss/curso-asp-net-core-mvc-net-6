e397b63fcf9062fe78333373d5ebd9590b2ff356
Vinicius Loss <viniciusvianaloss@gmail.com>
2023-10-21 02:35:40 -0300

    Iniciando novo projeto.

---
+++ 02_LachesMac/02_LachesMac/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _02_LachesMac.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
