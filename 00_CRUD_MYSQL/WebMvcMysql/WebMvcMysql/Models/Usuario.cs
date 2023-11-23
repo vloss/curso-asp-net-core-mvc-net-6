34ddf3b26244943e91b82701f3e04bc2d07c5213
Vinicius Loss <viniciusvianaloss@gmail.com>
2023-11-23 11:56:54 -0300

    Inicio de model.

---
+++ 00_CRUD_MYSQL/WebMvcMysql/WebMvcMysql/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace WebMvcMysql.Models
+{
+	public class Usuario
+	{
+
+		public int Id { get; set; }
+		public string Nome { get; set; }
+
+		public Usuario()
+		{
+		}
+	}
+}
+
