497f8ea12af704cf48ec8a707d4dff9a28ae3133
Vinicius Loss <viniciusvianaloss@gmail.com>
2023-11-24 09:46:38 -0300

    Backup.

--- CRUD_MVC_AVALICAO/CRUD_MVC_AVALICAO/Data/Contexto.cs
+++ CRUD_MVC_AVALICAO/CRUD_MVC_AVALICAO/Data/Contexto.cs
@@ -7,10 +7,24 @@ namespace CRUD_MVC_AVALICAO.Data
     public class Contexto : DbContext
     {
 
+        public Contexto() { }
+
         public Contexto(DbContextOptions<Contexto> options) : base(options)
         {
         }
 
         public DbSet<ProdutoModel> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+
+            //optionsBuilder.UseMySql("server=localhost;database=library;user=user;password=password");
+            optionsBuilder.UseMySql("Server=localhost;User Id=root;Password=root;Database=treinaweb");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { }
     }
 }
